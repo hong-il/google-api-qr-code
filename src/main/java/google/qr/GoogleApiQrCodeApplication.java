0a4c9d25a6861b11e59359b7b758d6a9b12d9337
hong-il <hongil.kim.dev@gmail.com>
2022-07-29 11:31:07 +0900

    Initial commit

---
+++ src/main/java/google/qr/GoogleApiQrCodeApplication.java
@@ -0,0 +1,13 @@
+package google.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApiQrCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleApiQrCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/google/qr/GoogleApiQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package google.qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApiQrCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
